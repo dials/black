8d1536ca408d57eeb61d8715f6160b4f16533646
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-02-27 13:32:33 +0000

    make setup python2 compatible

--- setup.py
+++ setup.py
@@ -1,36 +1,10 @@
 # Copyright (C) 2018 Łukasz Langa
-import ast
-import re
 from setuptools import setup
-import sys
-
-assert sys.version_info >= (3, 6, 0), "black requires Python 3.6+"
-from pathlib import Path  # noqa E402
-
-CURRENT_DIR = Path(__file__).parent
-
-
-def get_long_description() -> str:
-    readme_md = CURRENT_DIR / "README.md"
-    with open(readme_md, encoding="utf8") as ld_file:
-        return ld_file.read()
-
-
-def get_version() -> str:
-    black_py = CURRENT_DIR / "black.py"
-    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
-    with open(black_py, "r", encoding="utf8") as f:
-        match = _version_re.search(f.read())
-        version = match.group("version") if match is not None else '"unknown"'
-    return str(ast.literal_eval(version))
-
 
 setup(
     name="black",
-    version=get_version(),
+    version="18.9b0",
     description="The uncompromising code formatter.",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
     keywords="automation formatter yapf autopep8 pyfmt gofmt rustfmt",
     author="Łukasz Langa",
     author_email="lukasz@langa.pl",
@@ -39,7 +13,6 @@ setup(
     py_modules=["black", "blackd"],
     packages=["blib2to3", "blib2to3.pgen2"],
     package_data={"blib2to3": ["*.txt"]},
-    python_requires=">=3.6",
     zip_safe=False,
     install_requires=["click>=6.5", "attrs>=18.1.0", "appdirs", "toml>=0.9.4"],
     extras_require={"d": ["aiohttp>=3.3.2", "aiohttp-cors"]},
